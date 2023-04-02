0e2e8b4a3b0e51d461ff405822d871e1f3ca7efe
eduparlema <eparejalema@middlebury.edu>
2023-04-02 15:35:10 -0400

    Update game.py

--- src/game.py
+++ src/game.py
@@ -32,6 +32,9 @@ test_graph = {
     'Territory 3': ['Territory 1', 'Territory 2', 'Territory 4'],
     'Territory 4': ['Territory 2', 'Territory 3', 'Territory 1']
 }
+
+# Test 
+
 m = Map(test_graph)
 Test = Game(m, ["P1", "P2", "P3"])
 Test.setup_game()
