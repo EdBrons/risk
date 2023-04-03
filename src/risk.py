0b679740e0db733ab2282ef9dceffc500b655870
Ian Edward Brons <ianebrons@gmail.com>
2023-04-03 19:13:53 -0400

    get new armies method

--- src/risk.py
+++ src/risk.py
@@ -14,22 +14,36 @@ class Risk:
         self.random_setup = random_setup
         self.turn = 0
         self.graph, self.continents, self.continent_rewards, self.names = default_map()
-        self.state = self.make_state()
+        self.territories = self.make_state()
     def make_state(self):
         graph_size = len(self.graph.keys())
         territory = np.array([0, -1])
         territories = np.array([territory] * graph_size)
         return territories
     def number_of_territories(self):
-        return self.state.shape[0]
+        return self.territories.shape[0]
+    def get_player_territories(self, player_id):
+        return np.where(self.territories[:, 1] == player_id)[0]
+    def get_new_armies(self, player_id):
+        my_territories = self.get_player_territories(player_id)
+        new_armies = my_territories.shape[0] // 3
+        if new_armies < 3:
+            new_armies = 3
+        for c in self.continents:
+            owned = True
+            for t in self.continents[c]:
+                if t not in my_territories:
+                    owned = False
+            if owned:
+                new_armies += self.continent_rewards[c]
+        return new_armies
     def claim_territories(self):
         n_players = len(self.players)
         if self.random_setup:
-            # randomly assigns territories to players
             order = np.resize(np.arange(n_players), self.number_of_territories())
             np.random.shuffle(order)
-            self.state[:, 0] = np.ones(self.number_of_territories())
-            self.state[:, 1] = order
+            self.territories[:, 0] = np.ones(self.number_of_territories())
+            self.territories[:, 1] = order
         else:
             raise NotImplementedError("We haven't implemented player choice yet...")
     def place_armies(self):
@@ -38,16 +52,15 @@ class Risk:
         armies_to_place = players_to_initial_armies[n_players]
         if self.random_setup:
             for p in self.players:
-                my_territories = np.isin(element=self.state[:, 1], test_elements=p.id)
+                my_territories = np.isin(element=self.territories[:, 1], test_elements=p.id)
                 army_placement = np.ones(my_territories.sum())
                 for _ in range(armies_to_place):
                     army_placement[np.random.randint(army_placement.shape[0])] += 1
-                print(army_placement)
-                self.state[my_territories, 0] = army_placement
+                self.territories[my_territories, 0] = army_placement
         else:
             raise NotImplementedError("We haven't implemented player choice yet...")
 
 risk = Risk([RandomPlayer(0), RandomPlayer(1), RandomPlayer(2)])
 risk.claim_territories()
 risk.place_armies()
-print(risk.state)
+# print(risk.get_player_territories(1))
