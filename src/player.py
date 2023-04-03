bec7650ac3cf8fb5a017140965dafdfda035e448
Ian Edward Brons <ianebrons@gmail.com>
2023-04-03 19:51:26 -0400

    the ai is doing something

--- src/player.py
+++ src/player.py
@@ -1,9 +1,15 @@
-import random
+import numpy as np
 
 class Player:
     def __init__(self, id):
         self.id = id
+    def take_action(self, state, code, valid):
+        return -1
 
 class RandomPlayer(Player):
     def __init__(self, id):
         super().__init__(id)
+    def take_action(self, state, code, valid):
+        if code == 0:
+            return np.random.choice(valid)
+        return super().take_action(state, code, valid)
--- src/risk.py
+++ src/risk.py
@@ -1,4 +1,4 @@
-import random
+import math
 import numpy as np
 from maps import default_map
 from player import RandomPlayer
@@ -7,12 +7,14 @@ NO_OWNER = -1
 
 # Taken from https://github.com/civrev/RLRisk/blob/master/rlrisk/environment/risk.py
 class Risk:
-    def __init__(self, players, random_setup = True):
+    def __init__(self, players, random_setup = True, max_turns = math.inf):
         if len(players) < 2 or len(players) > 6:
             raise ValueError("2 <= len(players) <= 6")
         self.players = players
         self.random_setup = random_setup
+        self.max_turns = max_turns
         self.turn = 0
+        self.finished = False
         self.graph, self.continents, self.continent_rewards, self.names = default_map()
         self.territories = self.make_state()
     def make_state(self):
@@ -49,7 +51,7 @@ class Risk:
             self.territories[:, 1] = order
         else:
             raise NotImplementedError("We haven't implemented player choice yet...")
-    def place_armies(self):
+    def setup_armies(self):
         n_players = len(self.players)
         players_to_initial_armies = { 2: 40, 3: 35, 4: 30, 5: 25, 6: 20 }
         armies_to_place_base = players_to_initial_armies[n_players]
@@ -63,8 +65,43 @@ class Risk:
                 self.territories[my_territories, 0] = army_placement
         else:
             raise NotImplementedError("We haven't implemented player choice yet...")
+    def winner(self):
+        return np.array_equal(self.territories[:, 1], np.repeat(self.territories[0, 0], self.number_of_territories()))
+    def get_state(self):
+        return self.territories
+    def play(self):
+        n_players = len(self.players)
+        self.claim_territories()
+        self.setup_armies()
+        while not self.finished:
+            player_id = self.players[self.turn % n_players].id
+            self.turn += 1
 
-risk = Risk([RandomPlayer(0), RandomPlayer(1), RandomPlayer(2)])
-risk.claim_territories()
-risk.place_armies()
-# print(risk.get_player_territories(1))
+            self.recruitment_phase(player_id)
+            self.attack_phase(player_id)
+            if self.winner():
+                self.finished = True
+                break
+            self.fortify_phase(player_id)
+
+            if self.turn > self.max_turns:
+                self.finished = True
+                break
+    def recruitment_phase(self, player_id):
+        new_armies = self.get_new_armies(player_id)
+        self.place_armies(player_id, new_armies)
+    def place_armies(self, player_id, armies):
+        player = self.players[player_id]
+        for _ in range(armies):
+            valid = self.get_player_territories(player_id)
+            t = player.take_action(self.get_state(), 0, valid)
+            self.territories[t][0] += 1
+    def attack_phase(self, player_id):
+        pass
+    def fortify_phase(self, player_id):
+        pass
+
+
+risk = Risk([RandomPlayer(0), RandomPlayer(1), RandomPlayer(2)], max_turns=100)
+risk.play()
+print(risk.get_state())
