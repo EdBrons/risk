95644751ea487f1e821368d64ef6a94a9f5adc2c
Ian Edward Brons <ianebrons@gmail.com>
2023-04-03 19:28:46 -0400

    removed some stuff

--- src/tests.py
+++ src/tests.py
@@ -4,20 +4,6 @@ from player import RandomPlayer
 
 class TestRisk(unittest.TestCase):
 
-    def test_players_len(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-    
     def n_players(self, n):
         return [ RandomPlayer(i) for i in range(n) ]
     
