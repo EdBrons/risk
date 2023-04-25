053e6995ee3bdd3eb06e182c91a4ee3485c3e545
Ian Edward Brons <ianebrons@gmail.com>
2023-04-25 19:28:28 -0400

    some code for the env
    Co-authored-by: Eduardo Pareja Lema  <eduparlema@users.noreply.github.com>

--- riskenv.py
+++ riskenv.py
@@ -1,7 +1,8 @@
 import gymnasium as gym
-from gymnasium.spaces import Dict, Discrete, MultiDiscrete
+from gymnasium.spaces import Dict, Discrete, MultiDiscrete, Graph 
 from riskstate import *
 from maps import default_map
+import numpy as np 
 
 class RiskEnv(gym.Env):
     """Custom Environment that follows gym interface."""
@@ -10,45 +11,47 @@ class RiskEnv(gym.Env):
 
     def __init__(self, n_players=2):
         super().__init__()
+
         
         self.risk = new_game(n_players)
+        self.observation_unit_max = 10
+        self.n_players = n_players
 
         n_territories = self.risk.n_territories()
-        n_phases = 9
+        self.n_phases = len(Phases)
+        self.MAX_ARMIES = 5
 
-        self.action_space = Dict( { 
-            "Setup": Discrete(n_territories), 
-            "Recruitment": Discrete(n_territories),
-            "FirstAttack": MultiDiscrete([n_territories, n_territories]),
-            "ContinueAttack": Discrete(2),
-            "Reinforce": Discrete(2),
-            "SubsequentAttack": Discrete(n_territories),
-            "Fortify1": Discrete(n_territories),
-            "Fortify2": Discrete(n_territories),
-            "Fortify3": Discrete(2)
-        } )
+        self.action_space = Discrete( n_territories )
 
         self.observation_space = Dict( {
-            "Phase": Discrete(n_phases),
-            "Territories": MultiDiscrete([n_territories, 2])
+            #"Graph": Graph(node_space= Discrete(n_territories), edge_space= Discrete(7)),
+            "Phase": Discrete(self.n_phases),
+            "Territories": MultiDiscrete(np.array([np.array([self.MAX_ARMIES, n_players])]*n_territories))
         } )
 
     def get_observation(self):
-        return None
-    
-    def get_reward(self, old_state):
-        return 0
+        current_phase_index = Phases.index(type(self.risk).__name__)
+        return {
+            #"Graph": self.risk.graph,
+            "Phase": current_phase_index,
+            "Territory": np.array([np.array([territory, owner]) for territory, owner in self.risk.territories])
+        }
 
     def step(self, action):
         info = {} 
         res, state = self.risk.step(action)
-        old_state = self.risk
+        #If invalid move
+        if res == Move.INVALID:
+            self.reward -= 0
+        reward = 1 if self.risk.won else -1 if res.VALID else -5
         self.risk = state
-        return self.get_observation(), self.get_reward(old_state), self.risk.finished() , info
+        # self.reward += reward 
+        return (self.get_observation(), reward, self.risk.finished() , info)
 
     def reset(self):
-        ...
-        #return observation  # reward, done, info can't be included
+        self.risk = new_game(self.n_players)
+        self.reward = 0 
+     
 
     def render(self):
         ...
