f30c86d7292ff7648e315d8c635c849843509f3e
Ian Edward Brons <ianebrons@gmail.com>
2023-05-05 14:40:35 -0400

    painstaking tedious work

--- visualize.py
+++ visualize.py
@@ -1,6 +1,57 @@
 import pygame
 import os
 
+
+ImageLocations = {
+    "Alaska": [8, 47], 
+    "NW_Territory": [66, 33], 
+    "Greenland": [200, 1], 
+    "Alberta": [73, 94],
+    "Ontario": [129, 86], 
+    "Quebec": [179, 81], 
+    "W_US": [69, 139], 
+    "E_US": [118, 141], 
+    "C_America": [77, 202],
+    #South America
+    "Venezuela": [135, 269], 
+    "Brazil": [149, 295], 
+    "Peru": [123, 305], 
+    "Argentina": [158, 379],
+    #Africa
+    "N_Africa": [301, 263], 
+    "Egypt": [378, 278], 
+    "E_Africa": [416, 317], 
+    "Congo": [382, 358], 
+    "S_Africa": [385, 410], 
+    "Madagascar": [473, 432],
+    #Australia
+    "Indonesia": [602, 349], 
+    "New_Guinea": [682, 337], 
+    "W_Australia": [639, 410], 
+    "E_Australia": [700, 400],
+    #Europe
+    "Iceland": [300, 76], 
+    "Great_Britain": [268, 173], 
+    "Scandanavia": [353, 51],
+    "Ukraine": [400, 50], 
+    "N_Europe": [345, 127], 
+    "S_Europe": [355, 182], 
+    "W_Europe": [ 291, 180 ],
+    #Asia
+    "Mid_East": [414, 223], 
+    "Afghanistan": [476, 140], 
+    "Ural": [512, 28], 
+    "Siberia": [531, 2],
+    "Yakutsk": [ 607, 20 ], 
+    "Kamchatka": [647, 27], 
+    "Irkutsk": [593, 79], 
+    "Mongolia": [600, 128],
+    "Japan": [697, 127], 
+    "China": [554, 159], 
+    "India": [527, 217], 
+    "Siam": [604, 259]
+}
+
 # pygame setup
 pygame.init()
 
